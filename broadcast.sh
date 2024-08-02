ef9a6f64ac14d5ac7f79c3d4f9c38c14cd46d4a0
Keivan Ipchi Hagh <keivanipchihagh@gmail.com>
2024-08-02 21:18:02 +0330

    feat(telegram): Update broadcast script

--- broadcast.sh
+++ broadcast.sh
@@ -2,16 +2,30 @@
 
 # Check if both arguments are provided
 if [ $# -ne 3 ]; then
-    echo "Usage: $0 <API_TOKEN> <CHAT_ID> <CONFIG>"
+    echo "Usage: $0 <API_TOKEN> <CHAT_ID> <FILE> <N_ITEMS>"
     exit 1
 fi
 
 API_TOKEN="$1"
 CHAT_ID="$2"
-CONFIG="$3"
+FILE="$3"
 
-# Set the message text
-MESSAGE="$CONFIG"
+HEADER="V2ray Config:"
 
-# Use curl to send the message
-curl -s -X POST "https://api.telegram.org/bot$API_TOKEN/sendMessage" -d chat_id="$CHAT_ID" -d text="$MESSAGE"
+# Check if the file exists and read the top 10 lines
+if [ -f "$FILE" ]; then
+    count=0
+    while IFS= read -r line && [ $count -lt 1 ]; do
+        # Build the message
+        MESSAGE="${HEADER}%0A%0A${line}%0A%0AFollow us on: $CHAT_ID"
+
+        # Send each line as a separate message
+        curl -s -X POST "https://api.telegram.org/bot$API_TOKEN/sendMessage" \
+        -d chat_id="$CHAT_ID" \
+        -d text="$MESSAGE"
+        count=$((count + 1))
+    done < "$FILE"
+else
+    echo "File $FILE not found."
+    exit 1
+fi
