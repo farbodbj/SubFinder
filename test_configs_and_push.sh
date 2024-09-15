5bf6e46b49008f29467c8ec2e29eb3bbbdc3dc6f
farbod <farbod.bjary82@gmail.com>
2024-09-15 18:34:12 +0100

    fix-output-path

--- test_configs_and_push.sh
+++ test_configs_and_push.sh
@@ -9,11 +9,11 @@ export PATH=$PATH:/usr/local/go/bin
 bash docker-entrypoint.sh
 
 # Check if 'output.txt' has changed
-if git diff --exit-code output.txt > /dev/null; then
+if git diff --exit-code data/output.txt > /dev/null; then
     echo "No changes detected in output.txt. Skipping commit and push."
 else
     echo "Changes detected in output.txt. Committing and pushing changes."
-    git add output.txt
+    git add data/output.txt
     git commit -m "update output.txt, date: $(date)"
     git push origin master
 fi
