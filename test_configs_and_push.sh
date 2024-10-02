e79c5807323d69da4ed282708e31adee0e4face4
farbod <farbod.bjary82@gmail.com>
2024-10-02 20:01:37 +0100

    fix: stupid mistake :)

--- test_configs_and_push.sh
+++ test_configs_and_push.sh
@@ -6,8 +6,8 @@ cd "$(dirname "$0")"
 # Ensure Go is available in the cron job environment
 export PATH=$PATH:/usr/local/go/bin
 
-bash docker-entrypoint.sh
 git pull --rebase
+bash docker-entrypoint.sh
 
 # Check if 'output.txt' has changed
 if git diff --exit-code data/output.txt > /dev/null; then
